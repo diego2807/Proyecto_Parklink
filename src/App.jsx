c3f10d9260dc7577c98d1b4c903afc7a9f47fe7a
Esteban Fajardo <ramirezpazalejandra26@gmail.com>
2026-06-01 16:42:43 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {BrowserRoute, Routes, Route} from 'react-router-dom'
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <BrowserRoute>
+  <Routes>
+    <Route path='/vista' element={<Vista/>}/>
+  </Routes>
+  </BrowserRoute>
+  )
+}
+
+export default App
---
+++ src/vistas/Vista.jsx
@@ -0,0 +1,6 @@
+function Vista (){
+    return(
+        <h1>vista</h1>
+    )
+}
+export default Vista
